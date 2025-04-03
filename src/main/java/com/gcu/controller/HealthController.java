04b9d2415e1a0f73cdbb839f5e61de9952b163b8
Kai Hedrick <59582676+kaihedrick@users.noreply.github.com>
2025-04-03 09:28:00 -0700

    updated health controller

--- src/main/java/com/gcu/controller/HealthController.java
+++ src/main/java/com/gcu/controller/HealthController.java
@@ -1,15 +1,18 @@
 package com.gcu.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HealthController {
 
-    @GetMapping
-    public String healthCheck() {
+    @GetMapping("/")
+    public String home() {
         return "Application is running";
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
     }
 }
